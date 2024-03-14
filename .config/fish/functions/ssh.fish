41683b73e51bda5b1358a8c9abafd49e4a72c466
Frederik Palmø <frederik@palmoe.dk>
2024-03-14 03:10:59 +0100

    moved ssh config

---
+++ .config/fish/functions/ssh.fish
@@ -0,0 +1,4 @@
+function ssh --description 'alias ssh=ssh -F /home/vodofrede/.config/ssh/config -i /home/vodofrede/.config/ssh/id_rsa'
+ command ssh -F /home/vodofrede/.config/ssh/config -i /home/vodofrede/.config/ssh/id_rsa $argv
+        
+end
