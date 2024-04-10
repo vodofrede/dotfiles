30123df71e97e51ab0c735063d5d15f2e81bd638
Frederik Palmø <frederik@palmoe.dk>
2024-04-10 16:31:44 +0200

    removed custom ssh config

--- .config/fish/functions/ssh-copy-id.fish
+++
@@ -1,5 +0,0 @@
-function ssh-copy-id
-    mkdir $HOME/.ssh
-    command ssh-copy-id -F /home/vodofrede/.config/ssh/config -t ~/.config/ssh/authorized_keys $argv    
-    rmdir $HOME/.ssh || true
-end
--- .config/fish/functions/ssh.fish
+++
@@ -1,3 +0,0 @@
-function ssh
-    command ssh -F /home/vodofrede/.config/ssh/config $argv        
-end
